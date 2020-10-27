4dca508cdbbb4ad36320fb96f5288b5c574587f3
voron <voronina-liliya@mail.ru>
2020-10-27 22:32:56 +0300

    отдельный класс контакт

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname):
+        self.firstname=firstname
+        self.middlename=middlename
+        self.lastname=lastname
+        self.nickname=nickname

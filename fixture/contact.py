40c0a82e0d3523701488aafca79eb20a959eeca0
voron <voronina-liliya@mail.ru>
2020-11-02 00:23:17 +0300

    выделили класс для работы с контактами

---
+++ fixture/contact.py
@@ -0,0 +1,35 @@
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_home_page_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create(self, contact):
+        wd = self.app.wd
+        # fill group form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_home_page_contact()
+
+    def add_new_contact(self):
+        wd = self.app.wd
+        # add new contact
+        wd.find_element_by_link_text("add new").click()
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
+import pytest
 from model.contact import Contact
 from fixture.application import Application
-import pytest
 
 
 @pytest.fixture()
 def app(request):
-    fixture = Application ()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_contact()
-    app.create_contact(Contact(firstname="ewre", middlename="eww", lastname="ewew", nickname="ewewc"))
+    app.contact.add_new_contact()
+    app.contact.create(Contact(firstname="ewre", middlename="eww", lastname="ewew", nickname="ewewc"))
     app.session.logout()
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_contact()
-    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname=""))
+    app.contact.add_new_contact()
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname=""))
     app.session.logout()

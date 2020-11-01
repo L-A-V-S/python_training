f3cd2779d833ff0239e4e6beb4c8c8d7e43b38ff
voron <voronina-liliya@mail.ru>
2020-11-01 23:37:34 +0300

    созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 import pytest
 
 

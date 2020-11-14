249f467cabc48293f34bc62c48ec805d6df88c48
voron <voronina-liliya@mail.ru>
2020-11-14 15:39:08 +0300

    оптимизация

--- fixture/group.py
+++ fixture/group.py
@@ -15,7 +15,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        self.addinfo(group)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -27,13 +27,43 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def modify(self,group):
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # open modify form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+
+        # modify
+        self.fill_group_form(group)
+        # click update
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def modify_first_group(self,new_group_data):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
@@ -41,19 +71,7 @@ class GroupHelper:
         # click edit
         wd.find_element_by_name("edit").click()
         # modify
-        self.addinfo(group)
+        self.fill_group_form(new_group_data)
         # click update
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
-
-    def addinfo(self,group):
-        wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,3 +4,13 @@ def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify(Group(name="test2", header="modif2", footer="test2"))
     app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()

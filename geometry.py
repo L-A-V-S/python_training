573d037e436dafcd13c89ae96d08ccea020a1afd
voron <voronina-liliya@mail.ru>
2020-11-15 21:29:11 +0300

    geometry

--- geometry.py
+++ geometry.py
@@ -1,13 +1,15 @@
 from math import sqrt
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, _x, _y):
+        self.x = _x
+        self.y = _y
 
-def distance(p1,p2):
-    dx = p2.x - p1.x
-    dy = p2.y - p1.y
-    return sqrt(dx*dx +dy*dy)
+    def distance(self,p2):
+        dx = p2.x - self.x
+        dy = p2.y - self.y
+        return sqrt(dx*dx +dy*dy)
 
-print(distance(Point(0,0), Point(3,4)))
+a = Point(0,0)
+b = Point(3,4)
+print(a.distance(b))

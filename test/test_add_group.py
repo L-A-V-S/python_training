a51b65cfcd8e6fc04aef497fbd6ae69a1baa9917
voron <voronina-liliya@mail.ru>
2020-11-21 15:38:40 +0300

    проверки в тестах group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,5 +9,8 @@ def test_add_group(app):
     assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -7,12 +7,18 @@ def test_modify_group(app):
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)

a8ee726b70e261e89841af3ab741ffb88a7c92de
voron <voronina-liliya@mail.ru>
2020-11-21 18:43:58 +0300

    сравнение списков групп

--- model/group.py
+++ model/group.py
@@ -1,4 +1,5 @@
 
+from sys import maxsize
 
 class Group:
 
@@ -13,3 +14,9 @@ class Group:
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -8,11 +8,15 @@ def test_modify_group(app):
 
 def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.modify_first_group(Group(name="New group"))
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups =
+
 
 
 def test_modify_group_header(app):

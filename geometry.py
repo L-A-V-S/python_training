9e693860f38dd577846a41dc1ad1487556a78db9
voron <voronina-liliya@mail.ru>
2020-11-21 15:24:51 +0300

    get_group_list

--- geometry.py
+++
@@ -1,7 +0,0 @@
-from geom2d import *
-
-a = Point(0,0)
-b = Point(3,4)
-print(a.distance(b))
-print(a == b)
-print(a == Point(0,0))
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,7 +3,10 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="wer34t", header="wqert", footer="qwer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))

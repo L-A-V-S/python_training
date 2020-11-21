62124f6e74e78c3e653728cb888132c1668134c7
voron <voronina-liliya@mail.ru>
2020-11-21 23:51:41 +0300

    сортировка списков в методах contact

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,25 +4,29 @@ def test_modify_contact(app):
     old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    app.contact.modify(Contact(firstname="test", middlename="modify", lastname="mod", nickname="test"))
+    contact = Contact(firstname="test", middlename="modify", lastname="mod", nickname="test")
+    contact.id = old_contacts[0].id
+    app.contact.modify(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_modify_contact_firstname(app):
-    old_contacts = app.contact.get_contact_list()
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="test"))
-    app.contact.modify_first_contact(Contact(firstname="New firstname"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+#def test_modify_contact_firstname(app):
+#    old_contacts = app.contact.get_contact_list()
+#    if app.contact.count() == 0:
+#        app.contact.create(Contact(firstname="test"))
+#    app.contact.modify_first_contact(Contact(firstname="New firstname"))
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts) == len(new_contacts)
 
 
-def test_modify_contact_middlename(app):
-    old_contacts = app.contact.get_contact_list()
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="test"))
-    app.contact.modify_first_contact(Contact(middlename="New middlename"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+#def test_modify_contact_middlename(app):
+#    old_contacts = app.contact.get_contact_list()
+#    if app.contact.count() == 0:
+#        app.contact.create(Contact(firstname="test"))
+#    app.contact.modify_first_contact(Contact(middlename="New middlename"))
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts) == len(new_contacts)
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -21,8 +21,13 @@ def test_modify_group_name(app):
 
 #def test_modify_group_header(app):
 #    old_groups = app.group.get_group_list()
+#    group = Group(header="New header")
+#    group.id = old_groups[0].id
 #    if app.group.count() == 0:
 #        app.group.create(Group(name="test"))
-#    app.group.modify_first_group(Group(header="New header"))
+#    app.group.modify_first_group(group)
 #    new_groups = app.group.get_group_list()
 #    assert len(old_groups) == len(new_groups)
+#    old_groups[0] = group
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+

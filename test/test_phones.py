4dd4083ed3261c6a8d3ab6d22fea12812c671645
voron <voronina-liliya@mail.ru>
2020-12-13 20:12:51 +0300

    modify_by_index

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,8 +1,10 @@
-
+import re
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_home_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_home_page.mobilphone == contact_from_edit_page.mobilphone
-    assert contact_from_home_page.secondaryphone == contact_from_edit_page.secondaryphone
+    assert contact_from_home_page.homephone == clear(contact_from_edit_page.homephone)
+    assert contact_from_home_page.workphone == clear(contact_from_edit_page.workphone)
+    assert contact_from_home_page.mobilphone == clear(contact_from_edit_page.mobilphone)
+
+def clear(s) :
+    return re.sub("[() -]", "" , s)

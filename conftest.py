f5ed1e1f1f6bfc28500343271080a0d8cdae7f15
voron <voronina-liliya@mail.ru>
2020-12-28 14:06:08 +0300

    параметры из командной строки

--- conftest.py
+++ conftest.py
@@ -7,7 +7,9 @@ fixture = None
 def app(request):
     global fixture
     if fixture is None:
-        fixture = Application()
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application()
@@ -23,3 +25,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/")
--- fixture/application.py
+++ fixture/application.py
@@ -6,16 +6,24 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd == webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/")
+        wd.get(self.base_url)
 
     def is_valid(self):
         try:

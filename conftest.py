8f328761be617c6be59f065e514b5a4ecf16c580
voron <voronina-liliya@mail.ru>
2020-12-29 18:39:16 +0300

    генератор json

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 import os.path
+import importlib
 from fixture.application import Application
 
 fixture = None
@@ -32,3 +33,20 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+def load_from_json(file):
+    pass

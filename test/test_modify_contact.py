f34794709931ba2b1c32fa58c9eea6cd6098e3ac
voron <voronina-liliya@mail.ru>
2020-12-31 01:28:15 +0300

    переделываем тесты контактов 20 задание

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,15 +1,15 @@
 from model.contact import Contact
 from random import randrange
 
-def test_modify_contact(app):
-    if app.contact.count() == 0:
+def test_modify_contact(app, db):
+    if len(db.get_contact_list())  == 0:
         app.contact.create(Contact(firstname="test"))
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     contact = Contact(firstname="test", middlename="modify", lastname="mod", nickname="test")
     contact.id = old_contacts[index].id
     app.contact.modify_contact_by_index(index, contact)
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) != sorted(new_contacts, key=Contact.id_or_max)

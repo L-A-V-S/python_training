fa1355cd1cc8e92de349f819bdc469ad9352f814
voron <voronina-liliya@mail.ru>
2020-12-31 01:23:03 +0300

    переделываем тесты контактов 20 задание

--- fixture/contact.py
+++ fixture/contact.py
@@ -46,12 +46,30 @@ class ContactHelper:
         self.return_home_page_contact()
         self.contact_cache = None
 
+    def delete_contact_by_id(self, id):
+        wd = self.app.wd
+        self.open_contact_page()
+        # select first contact
+        self.select_contact_by_id(id)
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        # ok dialog window
+        wd.switch_to_alert().accept()
+        # wait info
+        wd.find_element_by_css_selector("div.msgbox")
+        self.return_home_page_contact()
+        self.contact_cache = None
+
     def delete_first_contact(self):
         self.delete_contact_by_index(0)
 
     def select_contact_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_contact_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def modify_contact_by_index(self, index, contact):
         wd = self.app.wd
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from random import randrange
+import random
 
-def test_delete_some_contact(app):
-    if app.contact.count() == 0:
+def test_delete_some_contact(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="test", middlename="test", lastname="test", nickname="test"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    app.contact.delete_contact_by_index(index)
-    new_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.delete_contact_by_id(contact.id)
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
-    old_contacts[index:index+1] = []
+    old_contacts.remove(contact)
     assert old_contacts == new_contacts
 

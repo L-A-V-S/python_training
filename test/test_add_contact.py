390a6d86c3938eb6e402447dda7014e2a6f7b6c1
voron <voronina-liliya@mail.ru>
2020-12-31 00:58:36 +0300

    переделываем тесты контактов 20 задание

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -25,7 +25,7 @@ def test_add_contact(app, db, json_contacts):
     new_contacts = db.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) != sorted(new_contacts, key=Contact.id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -11,12 +11,12 @@ from timeit import timeit
 
 
 
-#def test_group_list(app,db):
-#    print(timeit(lambda:app.group.get_group_list(), number=1))
-#    def clean(group):
-#        return Group(id=group.id, name=group.name.strip())
-#    print(timeit(lambda: map(clean,db.get_group_list()), number=1))
-#    assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+def test_group_list(app,db):
+    print(timeit(lambda:app.group.get_group_list(), number=1))
+    def clean(group):
+        return Group(id=group.id, name=group.name.strip())
+    print(timeit(lambda: map(clean,db.get_group_list()), number=1))
+    assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
 def test_contact_list(app,db):
    # print(timeit(lambda:app.contact.get_contact_list(), number=1))

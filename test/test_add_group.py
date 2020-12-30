5551d068ecb2e75e1cd4fb0b2647efdb361170ca
voron <voronina-liliya@mail.ru>
2020-12-30 13:19:51 +0300

    замена на дб в удалении групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,12 +7,11 @@ from model.group import Group
 
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
